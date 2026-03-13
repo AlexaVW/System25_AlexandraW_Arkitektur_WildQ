ac08ab5f4c78cd9250c1ba7848400f9567c95d6e
Alexandra Westas <alexandra.westas1999@hotmail.com>
2026-03-13 23:08:47 +0100

    Made the app mobile friendly

--- WildQ.Infrastructure/Data/MongoDb.cs
+++ WildQ.Infrastructure/Data/MongoDb.cs
@@ -13,7 +13,8 @@ namespace WildQ.Infrastructure.Data
     {
         public static MongoClient GetClient()
         {
-            const string connectionUri = "mongodb+srv://alexandrawestas1999_db_user:Quizquiz123@cluster0.ccnelh2.mongodb.net/?appName=Cluster0";
+            //const string connectionUri = "mongodb+srv://alexandrawestas1999_db_user:Quizquiz123@cluster0.ccnelh2.mongodb.net/?appName=Cluster0";
+            const string connectionUri = "mongodb://alexandrawestas1999_db_user:Quizquiz123@ac-9tpppfl-shard-00-00.ccnelh2.mongodb.net:27017,ac-9tpppfl-shard-00-01.ccnelh2.mongodb.net:27017,ac-9tpppfl-shard-00-02.ccnelh2.mongodb.net:27017/?ssl=true&replicaSet=atlas-3ovr6p-shard-0&authSource=admin&appName=Cluster0";
             var settings = MongoClientSettings.FromConnectionString(connectionUri);
             settings.ServerApi = new ServerApi(ServerApiVersion.V1);
             var client = new MongoClient(settings);
